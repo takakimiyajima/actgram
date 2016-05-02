19b713b5bd8bf9fa4c2ecf49d961803afb8adeaa
takakimiyajima <takaki.mz@icloud.com>
2016-05-02 03:56:28 +0000

    install the devise

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+    #if signed_in?
+     # @medias = Instagram.tag_recent_media(URI.encode("子ども"))
+    #end
   end
 
   def help
   end
+  
+  def about
+  end
+
 end
